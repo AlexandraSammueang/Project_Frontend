002e46dead0ed58ead92dca62ac3d2996f6a1c77
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-04-26 14:18:08 +0200

    Created tabs

--- Libery_Frontend/Libery_Frontend/App.xaml.cs
+++ Libery_Frontend/Libery_Frontend/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Libery_Frontend.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ Libery_Frontend/Libery_Frontend/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Libery_Frontend.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            newsPhoto.Source = $"https://images.pexels.com/photos/3953481/pexels-photo-3953481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+        }
+    }
+}

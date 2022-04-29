ab8245186e28c7c373218d00ae0480b285d10bc3
Adam <laashiri@gmail.com>
2022-04-30 01:35:30 +0200

    Added entity framework core to work with the database in azure.

---
+++ Libery_Frontend/Libery_Frontend/Views/Books.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Libery_Frontend.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Books : ContentPage
+    {
+        public List<Models.Product> Products;
+        public Books()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (var db = new Models.LibraryDBContext())
+            {
+                Products = db.Products.ToList();
+            }
+
+            ProductListView.ItemsSource = Products;
+        }
+    }
+
+    class ProductModel
+    {
+        public string Name { get; set; } = default;
+    }
+}

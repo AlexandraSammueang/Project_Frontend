8acec6b2e8c6f319006a5eb188412196cde11d24
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-05-01 21:50:33 +0200

    Fixed little bit in LibraryPage

--- Libery_Frontend/Libery_Frontend/Views/LibraryPage.xaml.cs
+++ Libery_Frontend/Libery_Frontend/Views/LibraryPage.xaml.cs
@@ -17,14 +17,35 @@ namespace Libery_Frontend.Views
                 "Recepten är enkla – de innehåller sällan fler än sju ingredienser och är klara på omkring 30 minuter – och precis som i Gustavs tidigare böcker är de förstås också jävligt goda." +
                 "Oavsett om du är ny i köket, redan lagar veganskt men vill hitta nya genvägar eller har velat prova men tyckt att det verkar krångligt så finns det något för dig i ”Vegansk hemkunskap”.";
            
-            //Photo2.Source = "http://img.tradera.net/images/026/473480026_aae509a8-d9fc-403a-8bdd-157019a66fb6.jpg";
-            //Photo3.Source = "https://static-se.bookis.com/books/497469/full.jpg";
+            //Photo2.Source = "https://images.axiellmedia.com/cover/1000178/1000178_202010071345.jpg?height=292";
+            //Book2.Text = "Jan Guillous debutroman Ondskan kom 1981 och räknas redan som en klassiker inom den moderna svenska litteraturen. " +
+            //    "Ondskan är en berättelse om våldet i den svenska vardagen, " +
+            //    "där vi får följa huvudpersonen Eriks fostran i ondska från hemmet med den sadistiske fadern till skolgården med dess gängbildningar.";
+            
+            
+            //Photo3.Source = "https://s3.eu-north-1.amazonaws.com/bookis-se.web.production/books/47360/full.jpg";
+            //Book3.Text = "Saruman har besegrats, men kampen är inte över.Mordors styrkor har nämligen siktats in sig på konungarnas gamla stad Minas Tirith i Gondor," +
+            //    " och ett slag verkar oundvikligt på Pelennors fält." +
+            //    "Samtidigt fortsätter Frodo, Sam och Gollum sin tröstlösa vandring i Mordor, över Gorgorothplattan för att förgöra ringen i Domberget." +
+            //    "Hela tiden måste de undvika Saurons öga som vakar i Barad - dûr.";
+
             //Photo4.Source = "https://s1.adlibris.com/images/54131167/harry-potter-och-hemligheternas-kammare.jpg";
+            //Book4.Text = "Sommarlovet är äntligen över! Harry Potter har längtat tillbaka till sitt andra år på Hogwarts skola för häxkonster och trolldom. " +
+            //    "Men hur ska han stå ut med den omåttligt stroppige professor Lockman? Vad döljer Hagrids förflutna? Och vem är egentligen Missnöjda Myrtle?" +
+            //    "De verkliga problemen börjar när någon, eller något, förstenar den ena Hogwartseleven efter den andra.Är det Harrys fiende, Draco Malfoy, som ligger bakom?" +
+            //    " Eller är det den som alla på Hogwarts misstänker -Harry Potter själv?";
+
             //Photo5.Source = "https://s1.adlibris.com/images/59846707/tim-biografin-om-avicii.jpg";
+            //Book5.Text = "Som ett fyrverkeri mot natthimlen exploderade DJ:n och producenten Avicii. En musikalisk visionär som genom sin känsla för melodier definierade epoken då svensk och europeisk housemusik tog över världen." +
+            //    "Men Tim Bergling var också en introvert och skör ung man som blev vuxen i ett omänskligt uppskruvat tempo." +
+            //    "Efter ett antal akuta sjukhusvistelser slutade han turnera sommaren 2016.Knappt två år senare tog han sitt liv.";
 
         }
 
-     
+        private void ReserveBook(object sender, System.EventArgs e)
+        {
+            (sender as Button).Text = "Du har nu bokat en bok ";
+        }
     }
     
 }

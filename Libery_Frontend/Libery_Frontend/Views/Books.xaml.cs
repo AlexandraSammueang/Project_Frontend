1e426df26a494d797a496c92e783500fb244f4d5
Adam <laashiri@gmail.com>
2022-05-04 02:53:46 +0200

    Products are loaded in asynchronously, resulting in a smoother navigation!

--- Libery_Frontend/Libery_Frontend/Views/Books.xaml.cs
+++ Libery_Frontend/Libery_Frontend/Views/Books.xaml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -23,32 +23,56 @@ namespace Libery_Frontend.Views
 
         }
 
-        protected override void OnAppearing()
+        protected  override void OnAppearing()
         {
             base.OnAppearing();
 
-            using (var db = new Models.LibraryDBContext())
-            {
-                
-                Products = db.Products.ToList();
-                ProdType = db.ProductTypes.ToList();
-
-                var prodType = Products.Join(ProdType, p => p.ProductTypeId, pi => pi.Id, (p, pi) => new { p, pi }).ToList();
-
-                ProductListView.ItemsSource = prodType;
-
-            }
-
-            
-
-
-
-
+            // Load products asynchronously
+            MainThread.BeginInvokeOnMainThread(async () => { ProductListView.ItemsSource = await GetProductsAsync(ActivityIndicator); });
         }
+
+        public async Task<List<ProductModel>> GetProductsAsync(ActivityIndicator indicator)
+        {
+            indicator.IsVisible = true;
+            indicator.IsRunning = true;
+            Task<List<ProductModel>> databaseTask = Task<List<ProductModel>>.Factory.StartNew(() =>
+            {
+                List<ProductModel> result = null;
+                try
+                {
+                    using (var db = new Models.LibraryDBContext())
+                    {
+
+                        Products = db.Products.ToList();
+                        ProdType = db.ProductTypes.ToList();
+
+                        result = Products.Join(ProdType, p => p.ProductTypeId, pi => pi.Id, (p, pi) => new ProductModel { Image = p.Image, Name = p.ProductName, Info = p.ProductInfo, Type = pi.Type }).ToList();
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    // Display modal for error
+                }
+                return result;
+            }
+            );
+
+            var taskResult =  await databaseTask;
+
+            indicator.IsRunning = false;
+            indicator.IsVisible = false;
+
+            return taskResult;
+        }
+
     }
 
-    class ProductModel
+    public class ProductModel
     {
+        public string Image { get; set; } = default;
         public string Name { get; set; } = default;
+        public string Info { get; set; } = default;
+        public string Type { get; set; } = default;
     }
 }

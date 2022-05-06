f5215f1b84d1aa25d4ff8d693fde90b05b2560a1
Shohruh <shohruh.bozorov@edu.newton.se>
2022-05-06 11:29:21 +0200

    Lade till öppettider på första sidan.

--- Libery_Frontend/Libery_Frontend/Views/AboutPage.xaml.cs
+++ Libery_Frontend/Libery_Frontend/Views/AboutPage.xaml.cs
@@ -16,6 +16,16 @@ namespace Libery_Frontend.Views
         {
             InitializeComponent();
             libraryPhoto.Source = $"https://static1biblioteket.stockholm.se/sites/default/files/Stadsbiblioteket-3.jpg";
+
+            labelTest.Text =
+                           "MÅN  9:00-18:00 \n" +
+                           "TIS  9:00-18:00 \n" +
+                           "ONS  9:00-18:00 \n" +
+                           "TOR  9:00-18:00 \n" +
+                           "FRE  9:00-18:00 \n" +
+                           "LÖR  9:00-18:00 \n" +
+                           "SÖN  9:00-18:00";
+
         }
 
         

9b7482c240d22408a856c7ff073f433dec3b97b2
teodor fredriksson <teo95@hotmail.se>
2022-05-18 10:30:42 +0200

    added class

---
+++ Libery_Frontend/Libery_Frontend/SecondModels/AuthorName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libery_Frontend.Views
+{
+    public class AuthorName
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int AuthorID { get; set; }
+        public override string ToString() => $"{Firstname} {Lastname}";
+
+
+
+    }
+}

45589fcde1d35deeb29eea4ba080e12897537271
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-05-19 13:37:07 +0200

    .

--- Libery_Frontend/Libery_Frontend/Models/Author.cs
+++ Libery_Frontend/Libery_Frontend/Models/Author.cs
@@ -16,7 +16,7 @@ namespace Libery_Frontend.Models
             
         }
 
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public DateTime? Birthday { get; set; }

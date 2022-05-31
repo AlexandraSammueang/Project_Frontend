40040ac35aeae823d5de6240f425b5270375c6a5
teodor fredriksson <teo95@hotmail.se>
2022-05-31 15:55:29 +0200

    added changes to aboutpage(homepage) added changes to password/register password entry

--- Libery_Frontend/Libery_Frontend/Views/AboutPage.xaml.cs
+++ Libery_Frontend/Libery_Frontend/Views/AboutPage.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,32 +7,54 @@ namespace Libery_Frontend.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutPage : ContentPage
-    {   
+    {
         public AboutPage()
         {
             InitializeComponent();
-            //libraryPhoto.Source = $"https://static1biblioteket.stockholm.se/sites/default/files/Stadsbiblioteket-3.jpg";
 
-            timeLabel.BackgroundColor = new Color (0.95,0.64, 0.38, 0.7);
             timeLabel.Text =
-                           "MÅN  10:00-16:00 \n" +
-                           "TIS  9:00-18:00 \n" +
-                           "ONS  9:00-19:00 \n" +
-                           "TOR  8:00-16:00 \n" +
-                           "FRE  10:00-17:00 \n" +
-                           "LÖR  11:00-15:00 \n" +
-                           "SÖN  12:00-15:00";
+                           "Öppettider \n\n\n" +
+                           "MÅN 10:00-16:00 \n" +
+                           "TIS 9:00-18:00 \n" +
+                           "ONS 9:00-19:00 \n" +
+                           "TOR 8:00-16:00 \n" +
+                           "FRE 10:00-17:00 \n" +
+                           "LÖR 11:00-15:00 \n" +
+                           "SÖN 12:00-15:00";
 
-          
+
         }
 
         private async void LogoutButton_Clicked(object sender, EventArgs e)
         {
-            await  Navigation.PopAsync();
+            await Navigation.PopAsync();
             var tab = new MainPage();
             tab.CurrentPage = tab.Children[0];
             await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(tab));
 
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Bra klickat", "Du har klickat på en bild", "OK");
+        }
+
+        private async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            var tab = new MainPage();
+            tab.CurrentPage = tab.Children[2];
+
+            await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(tab));
+        }
+
+        private async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+            await DisplayAlert("Bra klickat", "Du har klickat på en bild", "OK");
+        }
+
+        private async void TapGestureRecognizer_Tapped_3(object sender, EventArgs e)
+        {
+            await DisplayAlert("Bra klickat", "Du har klickat på en bild", "OK");
         }
     }
 }

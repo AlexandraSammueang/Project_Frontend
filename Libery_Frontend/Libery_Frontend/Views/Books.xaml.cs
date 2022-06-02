301d1366ca0543bbf359d80e8e3b31a5e82b2914
Ferri_Emin <ferri_emin@hotmail.com>
2022-06-02 15:48:15 +0200

    dubble search metod fix

--- Libery_Frontend/Libery_Frontend/Views/Books.xaml.cs
+++ Libery_Frontend/Libery_Frontend/Views/Books.xaml.cs
@@ -354,14 +354,5 @@ namespace Libery_Frontend.Views
             ProductListView.ItemsSource = booklist.Where(x => x.Name.ToLower().Contains(e.NewTextValue.ToLower()) || x.AuthorName.ToLower().Contains(e.NewTextValue.ToLower()));
             EbooksListview.ItemsSource = filmlist.Where(x => x.Name.ToLower().Contains(e.NewTextValue.ToLower()) || x.AuthorName.ToLower().Contains(e.NewTextValue.ToLower()));
         }
-
-        private async void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var booklist = await GetBooksAsync();
-            var filmlist = await GetMoviesAsync();
-
-            ProductListView.ItemsSource = booklist.Where(x => x.Name.ToLower().Contains(e.NewTextValue) || x.AuthorName.ToLower().Contains(e.NewTextValue));
-            EbooksListview.ItemsSource = filmlist.Where(x => x.Name.ToLower().Contains(e.NewTextValue) || x.AuthorName.ToLower().Contains(e.NewTextValue));
-        }
     }
 }
